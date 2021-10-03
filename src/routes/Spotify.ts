a6e4ca0f72cc47e451e699928f2db88030b3ec0a
Kyohei-us <kyohei1212000@gmail.com>
2021-10-03 04:01:55 -0700

    initial commit

---
+++ src/routes/Spotify.ts
@@ -0,0 +1,42 @@
+import StatusCodes from 'http-status-codes';
+import { Request, Response } from 'express';
+
+import UserDao from '@daos/User/UserDao.mock';
+import SpotifyWebApi from 'spotify-web-api-node';
+import { SpoUser } from '@server';
+
+const userDao = new UserDao();
+const { BAD_REQUEST, CREATED, OK } = StatusCodes;
+
+const CLIENT_ID = process.env.CLIENT_ID || ""
+const CLIENT_SECRET = process.env.CLIENT_SECRET || ""
+const OAUTH2_CALLBACK_URL = process.env.OAUTH2_CALLBACK_URL || ""
+
+/**
+ * Get current song.
+ * 
+ * @param req 
+ * @param res 
+ * @returns 
+ */
+export async function getCurrent(req: Request, res: Response) {
+    if (!req.user) return;
+    let reqUser = req.user as SpoUser;
+    let spotifyApi = new SpotifyWebApi({
+        clientId: CLIENT_ID,
+        clientSecret: CLIENT_SECRET,
+        redirectUri: OAUTH2_CALLBACK_URL,
+        accessToken: reqUser.accessToken,
+        refreshToken: reqUser.refreshToken
+    });
+    let ret = await spotifyApi.getMyCurrentPlayingTrack();
+    if (!(ret.body && ret.body.is_playing)) return;
+    if (ret.body.item && ret.body.item.uri && ret.body.item.name) {
+        const track = {
+            trackURI: ret.body.item.uri,
+            trackName: ret.body.item.name,
+        };
+        return res.status(OK).json({ track });
+    }
+    return res.status(404).json({ "message": "not found" })
+}
---
+++ src/routes/index.ts
@@ -0,0 +1,47 @@
+import express, { NextFunction, Request, Response, Router } from 'express';
+import { getAllUsers, addOneUser, updateOneUser, deleteOneUser } from './Users';
+import { getCurrent } from './Spotify';
+import passport from 'passport';
+
+// User-route
+const userRouter = Router();
+userRouter.get('/all', getAllUsers);
+userRouter.post('/add', addOneUser);
+userRouter.put('/update', updateOneUser);
+userRouter.delete('/delete/:id', deleteOneUser);
+
+
+const scope = [
+    "user-read-currently-playing",
+    "user-read-playback-state",
+];
+
+function ensureAuthenticated(req: Request, res: Response, next: NextFunction) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/');
+}
+
+// Spotify-route
+const spotifyRouter = Router();
+spotifyRouter.get('/auth/spotify', passport.authenticate('spotify', {
+    scope: scope,
+}));
+spotifyRouter.get(
+    '/auth/spotify/callback',
+    passport.authenticate('spotify', { scope: scope, failureRedirect: '/auth/spotify' }),
+    function (req, res) {
+        console.log("authed")
+        // Successful authentication, redirect home.
+        res.redirect('/');
+    }
+);
+spotifyRouter.get('/current', ensureAuthenticated, getCurrent);
+
+
+// Export the base-router
+const baseRouter = Router();
+baseRouter.use('/users', userRouter);
+baseRouter.use('/spotify', spotifyRouter)
+export default baseRouter;
