edf21c7d1042352e12a903a988b81d0c5b832298
Kyohei-us <kyohei1212000@gmail.com>
2021-10-08 02:17:56 -0700

    Add youtube-dl functionality

--- src/routes/Youtube.ts
+++ src/routes/Youtube.ts
@@ -1,6 +1,7 @@
 import { YTUser } from "@server";
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 import { Request, Response } from "express";
+import youtubedl, { YtResponse } from "youtube-dl-exec"
 
 export async function googleAuthCallback(req: Request, res: Response) {
     console.log(req.params.code);
@@ -40,18 +41,81 @@ export async function youtubeGetChannel(req: Request, res: Response) {
     }
 }
 
-export async function youtubeGetLikedVideos(req: Request, res: Response) {
+async function youtubeGetLikedVideos(req: Request, res: Response) {
     let reqUser = req.user as YTUser;
     let accessToken = reqUser.accessToken;
     let part = "contentDetails"
     let playlistURL = `https://www.googleapis.com/youtube/v3/videos?part=${part}&part=snippet&myRating=like`;
+    let config: AxiosRequestConfig = {
+        headers: { Authorization: `Bearer ${accessToken}` }
+    }
     let ret = await axios.get(
         playlistURL,
-        {
-            headers: { Authorization: `Bearer ${accessToken}` }
-        }
+        config
     );
+    return ret;
+}
+
+export async function youtubeGetLikedVideosWrapper(req: Request, res: Response) {
+    let ret = await youtubeGetLikedVideos(req, res);
     if (ret) {
         return res.status(200).json(ret.data)
+    }
+}
+
+export async function youtubeListVideoDLURLs(req: Request, res: Response) {
+    const myLikedVideos = await youtubeGetLikedVideos(req, res);
+    console.log(myLikedVideos.data)
+    let videoDLURLList: string[] = []
+    for (let i = 0; i < myLikedVideos.data.items.length; i++) {
+        const element = myLikedVideos.data.items[i];
+        let output = await youtubeGetDLURL(`${element.id}`);
+        if (typeof output === 'string') {
+            let url: string = output.split('\n')[0]
+            if (!url) {
+                res.redirect('/')
+            }
+            videoDLURLList.push(url);
+        } else {
+            res.redirect('/')
+        }
+    }
+    let html = `<html><body>`
+    for (let i = 0; i < videoDLURLList.length; i++) {
+        const element = videoDLURLList[i];
+        let atag = `<a href="${element}">DL</a>`
+        html += atag;
+    }
+    html += `</body></html>`;
+    res.send(html);
+    res.redirect('/')
+}
+
+async function youtubeGetDLURL(youtubeVideoId: string) {
+    const output: any = await youtubedl(`https://youtu.be/${youtubeVideoId}`, {
+        format: "best",
+        getUrl: true,
+    })
+    console.log(output)
+    return output;
+}
+
+export async function youtubeGetDLURLWrapper(req: Request, res: Response) {
+    let youtubeVideoId = req.params.video_id as string ? req.params.video_id as string : "";
+    if (!youtubeVideoId) {
+        res.redirect('/')
+    } else {
+        console.log(youtubeVideoId)
+        let output = await youtubeGetDLURL(youtubeVideoId);
+
+        if (typeof output === 'string') {
+            let url: string = output.split('\n')[0]
+            if (!url) {
+                res.redirect('/')
+            }
+            res.send(`<html><body><a href="${url}">DL</a></body></html>`);
+        } else {
+            res.redirect('/')
+        }
     }
 }
