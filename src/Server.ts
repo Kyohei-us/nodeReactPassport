057c95b0483db989b44c08cf8aae75d3868832ef
Kyohei-us <kyohei1212000@gmail.com>
2021-11-06 00:01:09 -0700

    Add ejs

--- src/Server.ts
+++ src/Server.ts
@@ -142,6 +142,7 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
 
 const viewsDir = path.join(__dirname, 'views');
 app.set('views', viewsDir);
+app.set('view engine', 'ejs');
 const staticDir = path.join(__dirname, 'public');
 app.use(express.static(staticDir));
 app.get('*', (req: Request, res: Response) => {
