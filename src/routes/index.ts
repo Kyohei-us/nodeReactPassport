36d58cd41f94ac8c5a55199a49565c3d18f3c97b
Kyohei-us <kyohei1212000@gmail.com>
2021-11-26 04:57:05 -0800

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -30,18 +30,6 @@ function ensureAuthenticated(req: Request, res: Response, next: NextFunction) {
     res.redirect('/api/youtube/auth/youtube')
 }
 
-function saveReturnTo(req: Request, res: Response, next: NextFunction) {
-    req.session.returnTo = req.get('Referrer');
-    console.log("return to:", req.session.returnTo);
-    return next()
-}
-
-// function auth(req: Request, res: Response, next: NextFunction) {
-//     res.setHeader('Access-Control-Allow-Origin', 'https://nifty-johnson-900cd2.netlify.app');
-//     res.setHeader('Access-Control-Allow-Credentials', 'true');
-//     return next()
-// }
-
 // Spotify-route
 // const spotifyRouter = Router();
 // spotifyRouter.get('/auth/spotify', passport.authenticate('spotify', {
@@ -63,11 +51,11 @@ function saveReturnTo(req: Request, res: Response, next: NextFunction) {
 
 // Youtube-route
 const youtubeRouter = Router();
-youtubeRouter.get('/auth/youtube', saveReturnTo, passport.authenticate('google', {
+youtubeRouter.get('/auth/youtube', passport.authenticate('google', {
     scope: ['profile', 'https://www.googleapis.com/auth/youtube.readonly']
 }));
 youtubeRouter.get('/auth/youtube/callback', passport.authenticate('google', {
-    failureRedirect: '/',
+    failureRedirect: 'https://nifty-johnson-900cd2.netlify.app',
     session: true
 }), googleAuthCallback);
 youtubeRouter.get('/getPlaylists', ensureAuthenticated, youtubeGetPlaylists)
